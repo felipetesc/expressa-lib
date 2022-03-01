c8156304f1bf59b15cc23cd92c8b79f9a71bb751
felipetesc <felipetesc@gmail.com>
2022-03-01 14:40:54 -0300

    removed files

--- mod.ts
+++
@@ -1,8 +0,0 @@
-import { AppConfigs, DatabaseConfigs } from "./utils/configs.ts";
-import { AssetsBufferizer, BlankFavicon} from "./utils/data.ts";
-import { Get, Post, IHandler, IRoute, AppContext, Handler, DefaultErrHandler } from "./handlers/handlers.ts";
-import { CreateServer, RunApp } from "./runner/runner.ts";
-import { Database, DBConn } from "./database/database.ts";
-
-export type {IHandler,IRoute, DBConn};
-export {AppConfigs, DatabaseConfigs, DefaultErrHandler, Handler, Get, Post, Database, AppContext, AssetsBufferizer, BlankFavicon, CreateServer, RunApp };
